64a016f7b57ed1db4e078252b601902f560aa3b9
priyanshu-purohit <purohitpriyanshu20@gmail.com>
2026-02-17 20:40:33 +0530

    Added get notifications api

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -7,6 +7,16 @@ const routes = (app) => {
         notificationMiddlewares.verifyTicketNotificationCreateRequest,
         notificationController.createTicket
     );
+
+    app.get(
+        '/notiservice/api/v1/notification/:id',
+        notificationController.getTicket
+    );
+    app.post(
+        '/notiservice/api/v1/notification',
+        notificationMiddlewares.verifyTicketNotificationCreateRequest,
+        notificationController.getAllTickets
+    );
 }
 
 module.exports = routes;
--- services/notification.service.js
+++ services/notification.service.js
@@ -22,6 +22,34 @@ const create = async (data) => {
     }
 }
 
+const getAll = async () => {
+    try{
+        const response = await Ticket.find();
+        return response;
+    }
+    catch(error){
+        throw error;
+    }
+}
+
+const getById = async (id) => {
+    try{
+        const response = await Ticket.findById(id);
+        if(!response){
+            throw {
+                err: 'No ticket details found',
+                code: STATUS.NOT_FOUND
+            }
+        }
+        return response;
+    }
+    catch(error){
+        throw error;
+    }
+}
+
 module.exports = {
-    create
+    create,
+    getAll,
+    getById
 }
